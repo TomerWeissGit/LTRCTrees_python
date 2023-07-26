5f3bd03d5214134fc3f9fe017b85b7e4c23c463f
TomerWeissGit <tomerweiss248@gmail.com>
2023-07-26 10:50:21 +0300

    log_rank_transformation function and SurvivalData class.

---
+++ LTRCSurvivalTree/__init__.py
@@ -0,0 +1,11 @@
+"""
+A Random Survival Forest implementation inspired by Ishwaran et al.
+With the addition of left truncation.
+Ishwaran, H., Kogalur, U. B., Blackstone, E. H., & Lauer, M. S. (2008).
+Random survival forests.
+The annals of applied statistics, 2(3), 841-860.
+
+"""
+
+from LTRCSurvivalTree.models import logranktrans, random_survival_forest_left_truncation
+from LTRCSurvivalTree import scoring
